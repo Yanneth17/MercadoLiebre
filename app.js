b100406da1314a794ac00b9a1f58e251cbaab2ad
Yanneth Carrasco <yanneth.carrasco@hotmail.com>
2022-01-30 17:03:00 -0500

    Edit app.js y package.json, para crear repo en heroku

--- app.js
+++ app.js
@@ -2,24 +2,24 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.listen(3004, function() { 
-    console.log("Hola, servidor funcionando")
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Servidor corriendo")
 });
 
-app.get('/home', (req,res) =>{
+app.get('/home', (req, res) => {
     res.send("Levantando un servidor");
 });
 
-app.use(express.static(path.join(__dirname,'./public')));
+app.use(express.static(path.join(__dirname, './public')));
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/index.html'))
-} )
+})
 
-app.get('/login', (req,res)=>{
+app.get('/login', (req, res) => {
     res.sendFile(__dirname + '/views/login.html');
 });
 
-app.get('/register', (req,res)=>{
+app.get('/register', (req, res) => {
     res.sendFile(__dirname + '/views/register.html');
 });
