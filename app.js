aa08585bf308bda3e52b5e05ed88dbcdd7d03db4
Yanneth Carrasco <yanneth.carrasco@hotmail.com>
2022-01-31 20:38:23 -0500

    Actualización puerto

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 3001, function() {
     console.log("Servidor corriendo")
 });
 
